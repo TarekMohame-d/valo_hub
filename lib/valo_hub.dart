3dfdbec03af53af352aa3964df0d8abafda5a64b
Tarek Mohamed <tarekmohamed1019@gmail.com>
2024-09-15 10:55:17 +0300

    - add app theme logic. - finish onboarding screen. - add home screen route.

--- lib/valo_hub.dart
+++ lib/valo_hub.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:valo_hub/core/routing/app_router.dart';
 import 'package:valo_hub/core/routing/routes.dart';
-import 'package:valo_hub/core/themes/app_colors.dart';
 import 'package:valo_hub/core/themes/app_themes.dart';
 
 class ValoHub extends StatelessWidget {
@@ -12,24 +10,6 @@ class ValoHub extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final isDarkTheme = AppThemes.isDarkMode;
-    SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(
-        statusBarColor: isDarkTheme
-            ? AppColors.scaffoldBlackColor
-            : AppColors.scaffoldLightColor,
-        statusBarIconBrightness:
-            isDarkTheme ? Brightness.light : Brightness.dark,
-        systemNavigationBarColor: isDarkTheme
-            ? AppColors.scaffoldBlackColor
-            : AppColors.scaffoldLightColor,
-        systemNavigationBarIconBrightness:
-            isDarkTheme ? Brightness.light : Brightness.dark,
-        systemNavigationBarDividerColor: isDarkTheme
-            ? AppColors.scaffoldBlackColor
-            : AppColors.scaffoldLightColor,
-      ),
-    );
     return ScreenUtilInit(
       designSize: const Size(375, 812),
       minTextAdapt: true,
